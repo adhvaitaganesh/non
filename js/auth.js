6bc2eb86ac5ffdb016d7184ea38ee650b0d82a9c
adhvaitaganesh <adhvaitaganesh@gmail.com>
2025-04-10 13:25:17 +0200

    Update GitHub Pages workflow and authentication

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@ function checkPassword() {
     const password = localStorage.getItem('sitePassword');
     if (!password) {
         const userPassword = prompt('Please enter the password to access this site:');
-        if (userPassword === 'YOUR_CHOSEN_PASSWORD') { // Replace with your desired password
+        if (userPassword === 'MRKD2025') { // Secure password for the site
             localStorage.setItem('sitePassword', userPassword);
             return true;
         } else {
